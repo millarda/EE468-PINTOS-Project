8e7bade6e42ca204252766543b369aa97f2e0e4d
millarda <akm.scout@gmail.com>
2018-09-26 15:15:40 -1000

    added basic syscalls

--- pintos/src/userprog/syscall.c
+++ pintos/src/userprog/syscall.c
@@ -7,14 +7,32 @@
 static void syscall_handler (struct intr_frame *);
 
 void
-syscall_init (void) 
+syscall_init (void)
 {
   intr_register_int (0x30, 3, INTR_ON, syscall_handler, "syscall");
 }
 
 static void
-syscall_handler (struct intr_frame *f UNUSED) 
+syscall_handler (struct intr_frame *f UNUSED)
 {
   printf ("system call!\n");
+  int callNum = 0;
+  //TODO have callNum be set to proper value
+  switch(callNum){
+    //TODO have case statements be proper cases
+    case 0://write
+    {
+      //TODO understand argument passing
+      break;
+    }
+    case 1://wait
+    {
+      //code required for Project B
+      //will need to be changed in future versions
+      while(1);
+      break;
+    }
+
+  }
   thread_exit ();
 }
