5f82ddacd011942608f0a602384a045b626e17e9
millarda <akm.scout@gmail.com>
2018-10-02 20:19:31 -1000

    added basis for write/wait (incomplete)

--- pintos/src/userprog/syscall.c
+++ pintos/src/userprog/syscall.c
@@ -1,16 +1,29 @@
+#include "devices/shutdown.h"
+#include "devices/input.h"
 #include "userprog/syscall.h"
+#include "userprog/process.h"
+#include "filesys/filesys.h"
+#include "filesys/file.h"
+#include "filesys/inode.h"
+#include "filesys/directory.h"
+#include "threads/palloc.h"
+#include "threads/malloc.h"
 #include <stdio.h>
 #include <syscall-nr.h>
 #include "threads/interrupt.h"
 #include "threads/thread.h"
 #include "threads/vaddr.h"
-#include "userprog/pagedir.h"
+#include "threads/synch.h"
+#include "lib/kernel/list.h"
+#include "vm/page.h"
 
 static void syscall_handler (struct intr_frame *);
 void sys_exit (int);
+void prep_pages(const void *buffer, size_t size);
+void unprep_pages(const void *buffer, size_t size);
 
-void 
-syscall_init (void) 
+void
+syscall_init (void)
 {
   intr_register_int (0x30, 3, INTR_ON, syscall_handler, "syscall");
 }
@@ -56,8 +69,27 @@ syscall_handler (struct intr_frame *f)
       NOT_REACHED();
       break;
     }
-#endif
-      
+
+  case SYS_WAIT:
+    {
+      while(1);
+    }
+
+  case SYS_WRITE:
+    {
+      int fd, return_code;
+      const void *buffer;
+      unsigned size;
+
+      memread_user(f->esp + 4, &fd, sizeof(fd));
+      memread_user(f->esp + 8, &buffer, sizeof(buffer));
+      memread_user(f->esp + 12, &size, sizeof(size));
+
+      return_code = sys_write(fd, buffer, size);
+      f->eax = (uint32_t) return_code;
+      break;
+    }
+
   /* unhandled case */
   default:
     printf("[ERROR] system call %d is unimplemented!\n", syscall_number);
@@ -74,7 +106,7 @@ oid sys_halt(void) {
 
 void sys_exit(int status) {
   printf("%s: exit(%d)\n", thread_current()->name, status);
-  
+
   // The process exits.
   // wake up the parent process (if it was sleeping) using semaphore,
   // and pass the return code.
@@ -104,4 +136,64 @@ is_valid_ptr(const void *user_ptr)
     return (pagedir_get_page(curr->pagedir, user_ptr)) != NULL;
   }
   return false;
+}
+
+int sys_write(int fd, const void *buffer, unsigned size) {
+  check_user((const uint8_t*) buffer);
+  check_user((const uint8_t*) buffer + size - 1);
+
+  lock_acquire (&filesys_lock);
+  int return_value;
+
+  if(fd == 1) { // for stdout
+    putbuf(buffer, size);
+    return_value = size;
+  }
+  else {
+    // for file
+    struct file_desc* fd_buf = find_file_desc(thread_current(), fd, FD_FILE);
+
+    if(fd_buf && fd_buf->file) {
+      prep_pages(buffer, size);
+      return_value = file_write(fd_buf->file, buffer, size);
+      unprep_pages(buffer, size);
+    }
+    else // no such file or can't open
+      return_value = -1;
+  }
+
+  lock_release (&filesys_lock);
+  return ret;
+}
+
+void prep_pages(const void *buffer, size_t size)
+{
+  struct supplemental_page_table *supt = thread_current()->supt;
+
+  uint32_t *pagedir = thread_current()->pagedir;
+
+  void *upage;
+  for(upage = pg_round_down(buffer); upage < buffer + size; upage += PGSIZE)
+  {
+    vm_load_page (supt, pagedir, upage);
+    vm_pin_page (supt, upage);
+  }
+}
+
+void unprep_pages(const void *buffer, size_t size)
+{
+  struct supplemental_page_table *supt = thread_current()->supt;
+
+  void *upage;
+  for(upage = pg_round_down(buffer); upage < buffer + size; upage += PGSIZE)
+  {
+    vm_unpin_page (supt, upage);
+  }
+}
+
+static void
+check_user (const uint8_t *uaddr) {
+
+  if(get_user (uaddr) == -1)
+    fail_invalid_access();
 }
