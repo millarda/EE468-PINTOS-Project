90fde3c05bb5fcb134211c22f571ee0c2b8b878e
Andrew Millard <akm.scout@gmail.com>
2018-10-06 17:33:58 -1000

    added thing

--- pintos/src/userprog/syscall.c
+++ pintos/src/userprog/syscall.c
@@ -16,6 +16,7 @@
 #include "threads/synch.h"
 #include "lib/kernel/list.h"
 #include "lib/user/syscall.h"
+#include "userprog/pagedir.h"
 // #include <unistd.h>
 // #include <sys/types.h>
 
@@ -26,10 +27,11 @@ void sys_exit (int);
 static void check_user (const uint8_t *uaddr);
 static int32_t get_user (const uint8_t *uaddr);
 bool is_valid_ptr(const void *user_ptr);
+void sys_halt();
 
 static int memread_user (void *src, void *dst, size_t bytes);
 enum fd_search_filter { FD_FILE = 1, FD_DIRECTORY = 2 };
-static struct file_desc* find_file_desc(struct thread *, int fd, enum fd_search_filter flag);
+// static struct file_desc* find_file_desc(struct thread *, int fd, enum fd_search_filter flag);
 struct file_descriptor * retrieve_file(int fd);
 
 struct file_descriptor{
@@ -37,10 +39,10 @@ struct file_descriptor{
   tid_t owner;
   struct file *file_struct;
   struct list_elem elem;
-}
+};
 
 struct lock fs_lock;
-
+// struct list open_files;
 struct lock filesys_lock;
 
 void
@@ -69,7 +71,7 @@ syscall_handler (struct intr_frame *f)
 
   // Store the esp, which is needed in the page fault handler.
   // refer to exception.c:page_fault() (see manual 4.3.3)
-  thread_current()->current_esp = f->esp;
+  //thread_current()->current_esp = f->esp;
   esp = f->esp;
   if(is_valid_ptr(esp)){
     exit(-1);
@@ -89,8 +91,8 @@ syscall_handler (struct intr_frame *f)
       int exitcode;
       //memread_user(f->esp + 4, &exitcode, sizeof(exitcode));
       is_valid_ptr(f->esp+1);
-      sys_exit(f->esp+1);
-      NOT_REACHED();
+      //sys_exit(f->esp+1);
+      //NOT_REACHED();
       break;
     }
 
@@ -106,22 +108,22 @@ syscall_handler (struct intr_frame *f)
 
   case SYS_TELL:
     {
-      int fd;
-      unsigned ret;
-
-      memread_user(f->esp + 4, &fd, sizeof(fd));
-
-      lock_acquire (&filesys_lock);
-      struct file_desc* file_d = find_file_desc(thread_current(), fd, FD_FILE);
-
-      if(file_d && file_d->file) {
-        ret = file_tell(file_d->file);
-      }
-      else
-        ret = -1;
-
-      lock_release (&filesys_lock);
-      f->eax = (uint32_t) ret;
+      // int fd;
+      // unsigned ret;
+      //
+      // memread_user(f->esp + 4, &fd, sizeof(fd));
+      //
+      // lock_acquire (&filesys_lock);
+      // struct file_desc* file_d = find_file_desc(thread_current(), fd, FD_FILE);
+      //
+      // if(file_d && file_d->file) {
+      //   ret = file_tell(file_d->file);
+      // }
+      // else
+      //   ret = -1;
+      //
+      // lock_release (&filesys_lock);
+      // f->eax = (uint32_t) ret;
       break;
     }
   case SYS_REMOVE:
@@ -140,10 +142,10 @@ syscall_handler (struct intr_frame *f)
     }
   case SYS_WRITE:
     {
-      if(is_valid_ptr((void*)(esp+1) && is_valid_ptr((void*)(esp+2) && is_valid_ptr((void*)(esp+3)))))
+      if(is_valid_ptr((const void*)(esp+1) && is_valid_ptr((const void*)(esp+2) && is_valid_ptr((const void*)(esp+3)))))
       {
-        if(is_valid_ptr((void*)(*(esp+2)) && is_valid_ptr((void*)(*(esp+2+(*esp+3)-1)))))
-          f->eax = (uint32_t) sys_write(*(esp+1), *(esp+2), *(esp+3));
+        if(is_valid_ptr((const void*)(*(esp+2)) && is_valid_ptr((const void*)(*(esp+2+(*esp+3)-1)))))
+          f->eax = (uint32_t) sys_write((int) *(esp+1), (const void) *(esp+2), (unsigned) *(esp+3));
         else{
           exit(-1);
         }
@@ -163,7 +165,7 @@ syscall_handler (struct intr_frame *f)
   }
 }
 
-void sys_halt(void) {
+void sys_halt() {
   shutdown_power_off();
 }
 
@@ -229,7 +231,7 @@ int sys_write(int fd, const void *buffer, unsigned size) {
 struct file_descriptor * retrieve_file(int fd){
   struct list_elem *list_element;
   struct file_descriptor *fd_struct;
-  for(list_element = list_head(&open_files); list_element != list_tail(&open_files);
+  for(list_element = list_head(&thread_current()->open_files); list_element != list_tail(&thread_current()->open_files);
   list_element = list_next(list_element)){
     fd_struct = list_entry (list_element, struct file_descriptor, elem);
     if (fd_struct->fd_num == fd)
@@ -252,34 +254,34 @@ check_user (const uint8_t *uaddr) {
     fail_invalid_access();
 }
 
-static struct file_desc*
-find_file_desc(struct thread *t, int fd, enum fd_search_filter flag)
-{
-  ASSERT (t != NULL);
-
-  if (fd < 3) {
-    return NULL;
-  }
-
-  struct list_elem *e;
-
-  if (! list_empty(&t->file_descriptors)) {
-    for(e = list_begin(&t->file_descriptors);
-        e != list_end(&t->file_descriptors); e = list_next(e))
-    {
-      struct file_desc *desc = list_entry(e, struct file_desc, elem);
-      if(desc->id == fd) {
-        // found. filter by flag to distinguish file and directorys
-        if (desc->dir != NULL && (flag & FD_DIRECTORY) )
-          return desc;
-        else if (desc->dir == NULL && (flag & FD_FILE) )
-          return desc;
-      }
-    }
-  }
-
-  return NULL; // not found
-}
+// static struct file_desc*
+// find_file_desc(struct thread *t, int fd, enum fd_search_filter flag)
+// {
+//   ASSERT (t != NULL);
+//
+//   if (fd < 3) {
+//     return NULL;
+//   }
+//
+//   struct list_elem *e;
+//
+//   // if (! list_empty(&t->file_descriptors)) {
+//   //   for(e = list_begin(&t->file_descriptors);
+//   //       e != list_end(&t->file_descriptors); e = list_next(e))
+//   //   {
+//   //     struct file_desc *desc = list_entry(e, struct file_desc, elem);
+//   //     if(desc->id == fd) {
+//   //       // found. filter by flag to distinguish file and directorys
+//   //       if (desc->dir != NULL && (flag & FD_DIRECTORY) )
+//   //         return desc;
+//   //       else if (desc->dir == NULL && (flag & FD_FILE) )
+//   //         return desc;
+//   //     }
+//   //   }
+//   // }
+//
+//   return NULL; // not found
+// }
 
 static int32_t
 get_user (const uint8_t *uaddr) {
